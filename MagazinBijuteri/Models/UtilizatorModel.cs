4c781fc073b284c55c71ca89b7750cf6fc555db8
simonamold <moldovan.simona96@gmail.com>
2023-01-18 12:31:35 +0200

    Add project files.

---
+++ MagazinBijuteri/Models/UtilizatorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagazinBijuteri.Models
+{
+    public class UtilizatorModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public string Prenume { get; set; } 
+        public string Parola { get; set; }
+    }
+
+    public class UtilizatorDbContext : DbContext
+    { 
+        public DbSet<UtilizatorModel> Utilizatori { get; set; }
+    }
+
+}
