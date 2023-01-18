ff47670fc3a02166e867ad8de8cace80b38b4f7f
Gabriela Muraru <114652559+GabrielaMuraru@users.noreply.github.com>
2023-01-18 15:24:15 +0200

    Add files via upload

---
+++ MagazinBijuteri/Controllers/ProdusController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MagazinDeBijuteriiPROIECTFINAL.Models;
+
+namespace MagazinDeBijuteriiPROIECTFINAL.Controllers
+{
+    public class ProdusController : Controller
+    {
+        private ProdusContext dbCtx = new ProdusContext();
+
+        // GET: Produs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ProdusModel std = new ProdusModel();
+            std.ProdusId = 0;
+            std.Denumire = "Denumire";
+            std.Descriere = "Descriere";
+            std.Cantitate = 0;
+
+
+            return View(std);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProdusModel std)
+        {
+            if (ModelState.IsValid)
+            {
+                dbCtx.Produse.Add(std);
+                dbCtx.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(std);
+        }
+    }
+}
